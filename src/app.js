2af1433aed751fe7866dcb8823dc7d1fa38ef5fc
ramkrishna mondal <ramkrishnam170@gmail.com>
2025-09-07 13:51:59 +0530

    mongodb updated to atlas

--- src/app.js
+++ src/app.js
@@ -8,13 +8,12 @@ import userRoute from './routes/admin.routes.js';
 const app = express();
 
 app.use(
-    cors({
-        origin: [
-      "http://localhost:5173",
-      "https://bio-lab-6ifn.vercel.app",
-    ],
-        credentials: true
-    })
+  cors({
+    origin: process.env.NODE_ENV === "production"
+      ? process.env.FRONTEND_URL
+      : ["http://localhost:5173"],
+    credentials: true,
+  })
 );
 
 app.use(express.json({ limit: "16kb" }));
